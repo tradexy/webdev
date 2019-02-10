183990547208d117a84ac070700409b2e9b3b9db
John Mosquera <tradexy@gmail.com>
2019-02-10 19:54:18 +0000

    graphs on project 2 and other earlier updates

---
+++ projects/second-project/graph5/js/index.js
@@ -0,0 +1,93 @@
+
+var chart, chartData, dataView;
+
+var columns = [
+  {
+    type: 'number',
+    label: 'month'
+  },
+  {
+    type: 'number',
+    label: 'A',
+    color: 'red',
+    disabledColor: '#FFD9D9',
+    visible: true
+  },
+  {
+    type: 'number',
+    label: 'B',
+    color: 'blue',
+    disabledColor: '#D9D9FF',
+    visible: true,
+  },
+  {
+    type: 'number',
+    label: 'C',
+    color: 'yellow',
+    disabledColor: '#C3E6C3',
+    visible: true,
+  }
+];
+
+var nullFunc = function() {return null;};
+
+google.load("visualization", "1", {packages: ["corechart"]});
+google.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+  if (!chart) {
+    chartData = google.visualization.arrayToDataTable([
+      ['month', 'A', 'B', 'C'],
+      [1, 2, 3, 4],
+      [2, 3, 1, 5],
+      [3, 1, 4, 2],
+      [4, 2, 2, 1],
+      [5, 3, 1, 5],
+      [6, 3, 1, 1],
+      [7, 3, 6, 2]
+    ],
+                                                      false
+                                                     );
+    dataView = new google.visualization.DataView(chartData);
+
+    chart = new google.visualization.LineChart(document.getElementById('chart'));
+
+    // Toggle visibility of data series on click of legend.
+    google.visualization.events.addListener(chart, 'click', function (target) {
+      if (target.targetID.match(/^legendentry#\d+$/)) {    
+        var index = parseInt(target.targetID.slice(12)) + 1;
+        columns[index].visible = !columns[index].visible;
+        drawChart();
+      }
+    });
+  }
+
+  var visibleColumnIndexes = [0];
+  var colors = [];
+
+  for (var i = 1; i < columns.length; i++) {
+    if (columns[i].visible) {
+      colors.push(columns[i].color);
+
+      visibleColumnIndexes.push(i);
+    }
+    else {
+      colors.push(columns[i].disabledColor);
+
+      visibleColumnIndexes.push({
+        calc: nullFunc,
+        type: columns[i].type,
+        label: columns[i].label,
+      });
+    }
+  };
+  dataView.setColumns(visibleColumnIndexes);
+
+  chart.draw(
+    dataView,
+    {
+      colors: colors
+    }
+  );
+};
