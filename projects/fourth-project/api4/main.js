c240047e54f6b1dea4c4d4c68a07db08d0e16617
John Mosquera <tradexy@gmail.com>
2019-02-26 21:26:10 +0000

    fourth project api json ajax

--- projects/fourth-project/api4/main.js
+++ projects/fourth-project/api4/main.js
@@ -3,7 +3,7 @@ var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('Get', 'https://raw.githubusercontent.com/tradexy/api/master/fx25feb19.json');
+  ourRequest.open('Get', 'https://forex.1forge.com/1.0.3/quotes?pairs=EURUSD,USDGBP,USDCNH,USDJPY,USDEUR&api_key=UQfSlMDB9vZUevh4DvY3Zu5zrga4LCW7');
 //ORIGINAL USE  ourRequest.open('Get', 'https://forex.1forge.com/1.0.3/quotes?pairs=EURUSD,USDGBP,USDCNH,USDJPY,USDEUR&api_key=UQfSlMDB9vZUevh4DvY3Zu5zrga4LCW7');
 //ourRequest.open('Get', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json'); GOOG name
 //ourRequest.open('Get', 'https://jsonplaceholder.typicode.com/posts');  GOOD  title
