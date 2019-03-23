1bcdf9cbc06d60d3398dbe80ec8ec3ea75a6f5be
John Mosquera <tradexy@gmail.com>
2019-03-23 11:42:31 +0000

    react and multiple

---
+++ projects/fifth-project/telmosinglepage/src/App.js
@@ -0,0 +1,30 @@
+//always need to have below for react
+import React, { Component } from 'react';
+//below is to enable using different tabs - with react-router-dom download ----
+import {BrowserRouter, Route} from 'react-router-dom';
+import './css/style.css';
+
+import Navbar from './components/navbar';
+import Home from './components/home';
+import ActorsContainer from './components/actorsContainer';
+import Actress from './components/actress';
+import Films from './components/films';
+
+class App extends Component {
+    
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" render={() => <Home title="Oscar Winners" />} />
+                    <Route path="/actors/" render={() => <ActorsContainer title="Best Actors" />} />
+                    <Route path="/actress/" render={() => <Actress title="Best Actress" />} />
+                    <Route path="/films" render={() => <Films title="Best Films" />} />
+                </div>
+            </BrowserRouter>
+        );
+  }
+}
+
+export default App;
---
+++ projects/fifth-project/telmosinglepage/src/data/actress.js
@@ -0,0 +1,50 @@
+export const ActressList = [
+    {
+    name: "Mika Hakkinen",
+    description: "Formula One Champion",
+    img_src: "https://seura.fi/wp-content/uploads/2016/08/Bulls_000b5cn9-1.jpg",
+    profile_img: "https://seura.fi/wp-content/uploads/2016/08/SKI0594110-1-1024x1014.jpg",
+    url: "Mikka-Hakkine"
+    
+    },
+    {
+    name: "Ilpo Musto",
+    description: "Top world striker, plays for LOPS",
+    img_src: "https://img-aws.ehowcdn.com/340x221p/photos.demandstudios.com/getty/article/3/19/135444616.jpg",
+    profile_img: "https://media.timeout.com/images/104623582/630/472/image.jpg",
+    url: "Ilpo-Musto"
+    },
+    {
+        name: "Lionel Messi",
+        description: "Another top world striker, plays for Barcelona FC",
+        img_src: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAvRbic.img?h=469&w=624&m=6&q=60&o=f&l=f&x=436&y=279",
+        profile_img: "https://images.performgroup.com/di/library/GOAL/1b/b6/lionel-messi-alaves_d97d0ajiubvl13zop6yravgr8.jpg?t=1325531557",
+        url: "Lionel-Messi"
+        
+        },
+        {
+        name: "David Beckham",
+        description: "Another world famous football player",
+        img_src: "http://www2.pictures.zimbio.com/gi/David+Beckham+Chivas+USA+v+Los+Angeles+Galaxy+L3d_xJuSX39l.jpg",
+        profile_img: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBNBHQW.img?h=768&w=1366&m=6&q=60&o=f&l=f&x=967&y=846",
+        url: "David-Beckham"
+        },
+        {
+            name: "Mike Tyson",
+            description: "Mike Tyson heavy weight champion of the World",
+            img_src: "http://dailydsports.com/wp-content/uploads/2015/07/20150630-4-tyson.jpg",
+            profile_img: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2019/02/rexfeatures_1289918af-920x584.jpg",
+            url: "Mike-Tyson"
+            
+            },
+            {
+            name: "Serena Williams",
+            description: "Serena Williams is a Grand Slam Champion",
+            img_src: "https://assets.teenvogue.com/photos/57c03d8e9b60a7db09380d0e/16:9/w_1280,c_limit/serena-williams.jpg",
+            profile_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Serena_Williams_at_2013_US_Open.jpg/220px-Serena_Williams_at_2013_US_Open.jpg",
+            url: "Serena-Williams"
+            },
+    
+    
+        
+    ];
